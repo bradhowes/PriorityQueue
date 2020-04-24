fe7bbcfabeba0f729c07aabfa44ff5720fb17786
Brad Howes <bradhowes@mac.com>
2020-04-24 13:24:57 +0200

    Lint cleanup

--- Package.swift
+++ Package.swift
@@ -3,17 +3,16 @@
 
 import PackageDescription
 
+
 let package = Package(
     name: "PriorityQueue",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "PriorityQueue",
-            targets: ["PriorityQueue"]),
+            targets: ["PriorityQueue"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
