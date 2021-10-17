88934e8fd74f9a55e5d66adb938a4ea10b883d70
Brad Howes <bradhowes@mac.com>
2021-10-18 00:29:35 +0200

    Minor cleanup and bug fixing.

--- Sources/PriorityQueue/PriorityQueue.swift
+++ Sources/PriorityQueue/PriorityQueue.swift
@@ -5,188 +5,220 @@
  contain 1 child). Each parent appears in the container before its children, ordered by a provided function.
  */
 public struct PriorityQueue<T> {
-    public typealias ElementType = T
+  public typealias ElementType = T
 
-    /// Defines a function type that returns true if the two areguments are considered in order.
-    public typealias IsOrderedOp = (ElementType, ElementType) -> Bool
-    /// Obtain the number of items currently in the queue
-    public var count: Int { heap.count }
-    /// Determine if the container is empty
-    public var isEmpty: Bool { heap.isEmpty }
-    /// Return the first item in the queue if there is one. Does not remove it from the queue.
-    public var first: ElementType? { heap.first }
+  /// Defines a function type that returns true if the two arguments are considered in order.
+  public typealias IsOrderedOp = (ElementType, ElementType) -> Bool
+  /// Obtain the number of items currently in the queue
+  public var count: Int { heap.count }
+  /// Determine if the container is empty
+  public var isEmpty: Bool { heap.isEmpty }
+  /// Return the first item in the queue if there is one. Does not remove it from the queue.
+  public var first: ElementType? { heap.first }
 
-    private let isOrdered: IsOrderedOp
-    private var heap: [ElementType] = []
+  private let isOrdered: IsOrderedOp
+  private var heap: [ElementType] = []
 
-    /**
-     Intialize new instance with zero or more items
-     - parameter compare: function that determines ordering of items in the queue elements in ascending order
-     - parameter args: zero or more items to add to queue
-     */
-    public init(compare: @escaping IsOrderedOp, _ args: ElementType...) {
-        self.isOrdered = compare
-        args.forEach { push($0) }
+  /**
+   Initialize new instance with zero or more items
+   - parameter compare: function that determines ordering of items in the queue elements in ascending order
+   - parameter args: zero or more items to add to queue
+   */
+  public init(compare: @escaping IsOrderedOp, _ args: ElementType...) {
+    self.isOrdered = compare
+    args.forEach { push($0) }
+  }
+
+  /**
+   Initialize new instance with a collection of items
+
+   - parameter compare: function that determines the ordering of itesm int the queue
+   - parameter values: collection of items to add
+   */
+  public init(_ compare: @escaping IsOrderedOp, values: [ElementType]) {
+    self.isOrdered = compare
+    values.forEach { push($0) }
+  }
+}
+
+extension PriorityQueue : CustomStringConvertible {
+  public var description: String { heap.description }
+}
+
+public extension PriorityQueue {
+
+  /**
+   Add a new item to the queue while maintaining binary heap property.
+   - parameter item: the new item to add
+   */
+  mutating func push(_ item: ElementType) {
+    heap.append(item)
+    shiftUp(at: heap.count - 1)
+  }
+
+  /**
+   Remove the first item from the queue and return it.
+   - returns: first item or nil if queue is empty
+   */
+  mutating func pop() -> ElementType? {
+    switch count {
+    case 0: return nil
+    case 1: return heap.removeLast()
+    default:
+      let value = heap[0]
+      heap[0] = heap.removeLast()
+      shiftDown(at: 0, until: heap.count)
+      return value
+    }
+  }
+
+  /**
+   Remove the item at the given index.
+
+   - parameter index: the position to remove
+   - returns: the item that was at the given position
+   */
+  mutating func remove(at index: Int) -> ElementType? {
+    guard index < heap.count else { return nil }
+
+    let last = heap.count - 1
+    if index != last {
+      heap.swapAt(index, last)
+      shiftDown(at: index, until: last)
+      shiftUp(at: index)
     }
 
-    /**
-     Intialize new instance with a collection of items
+    return heap.removeLast()
+  }
 
-     - parameter compare: function that determines the ordering of itesm int the queue
-     - parameter values: collection of items to add
-     */
-    public init(_ compare: @escaping IsOrderedOp, values: [ElementType]) {
-        self.isOrdered = compare
-        values.forEach { push($0) }
-    }
+  /// Remove all entries from the queue
+  mutating func removeAll() { heap.removeAll() }
 
-    /**
-     Add a new item to the queue while maintaining binary heap property.
-     - parameter item: the new item to add
-     */
-    public mutating func push(_ item: ElementType) {
-        heap.append(item)
-        siftDown(start: 0, index: heap.count - 1)
-    }
+  /**
+   Replace a value at a given position with a new one.
 
-    /**
-     Remove the first item from the queue and return it
-     - returns: first item or nil if queue is empty
-     */
-    public mutating func pop() -> ElementType? {
-        switch count {
-        case 0: return nil
-        case 1: return heap.removeLast()
-        default:
-            defer { siftUp(index: 0) }
-            heap.swapAt(0, heap.endIndex - 1)
-            return heap.removeLast()
-        }
-    }
+   - parameter index: the index of the item to replace
+   - parameter value: the new value to store
+   - returns: the previous item at the given index
+   */
+  mutating func replace(at index: Int, with value: T) -> ElementType? {
+    guard index < heap.count else { return nil }
+    defer { push(value) }
+    return remove(at: index)
+  }
 
-    /// Remove all entries from the queue
-    public mutating func removeAll() { heap.removeAll() }
+  /**
+   Replace the root with a new value.
+
+   - parameter value: new value to use
+   - returns: old root value
+   */
+  mutating func replace(_ value: ElementType) -> ElementType? {
+    return replace(at: 0, with: value)
+  }
 }
 
 extension PriorityQueue {
 
-    public typealias ForEachBlockType = (ElementType) -> Void
+  public typealias ForEachBlockType = (ElementType) -> Void
 
-    /**
-     Allow destructive iteration over the queue, handing the next ordered element the queue to the given closure.
-     - parameter block: the closure to call to process the element
-     */
-    public mutating func forEach(block: ForEachBlockType) {
-        while let element = pop() {
-            block(element)
-        }
+  /**
+   Allow destructive iteration over the queue, handing the next ordered element the queue to the given closure.
+   - parameter block: the closure to call to process the element
+   */
+  public mutating func forEach(block: ForEachBlockType) {
+    while let element = pop() {
+      block(element)
     }
+  }
 }
 
-extension PriorityQueue {
+private extension PriorityQueue {
 
-    private func isOrdered(_ left: Int, _ right: Int) -> Bool { isOrdered(heap[left], heap[right]) }
+  func isOrdered(_ left: Int, _ right: Int) -> Bool { isOrdered(heap[left], heap[right]) }
 
-    private mutating func siftUp(index: Int) {
-        var pos = index
-        let startPos = pos
-        let newItem = heap[pos]
-        var childPos = 2 * pos + 1
-        while childPos < heap.count {
-            let rightPos = childPos + 1
-            if rightPos < heap.count && !isOrdered(childPos, rightPos) {
-                childPos = rightPos
-            }
-            heap[pos] = heap[childPos]
-            pos = childPos
-            childPos = 2 * pos + 1
-        }
+  mutating func shiftUp(at index: Int) {
+    var pos = index
+    let child = heap[pos]
+    var parentPos = (pos - 1) >> 1
 
-        heap[pos] = newItem
-        siftDown(start: startPos, index: pos)
+    while pos > 0 && isOrdered(child, heap[parentPos]) {
+      heap[pos] = heap[parentPos]
+      pos = parentPos
+      parentPos = (pos - 1) >> 1
     }
 
-    private mutating func siftDown(start: Int, index: Int) {
-        let newItem = heap[index]
-        var pos = index
-        while pos > start {
-            let parentPos = (pos - 1) >> 1
-            let parent = heap[parentPos]
-            if isOrdered(newItem, parent) {
-                heap[pos] = parent
-                pos = parentPos
-                continue
-            }
-            break
-        }
-        heap[pos] = newItem
-    }
+    heap[pos] = child
+  }
+
+  mutating func shiftDown(at index: Int, until: Int) {
+    let leftPos = index * 2 + 1
+    let rightPos = leftPos + 1
+
+    var first = index
+    if leftPos < until && isOrdered(leftPos, first) { first = leftPos }
+    if rightPos < until && isOrdered(rightPos, first) { first = rightPos }
+    if first == index { return }
+
+    heap.swapAt(index, first)
+    shiftDown(at: first, until: until)
+  }
 }
 
 extension PriorityQueue where ElementType: Equatable {
 
-    /**
-     Determine if container holds the given value.
+  /**
+   Determine if container holds the given value.
 
-     - parameter value: the value to look for
-     - returns: true if found
-     */
-    public func contains(_ value: ElementType) -> Bool { heap.contains(value) }
+   - parameter value: the value to look for
+   - returns: true if found
+   */
+  public func contains(_ value: ElementType) -> Bool { heap.contains(value) }
 }
 
 extension PriorityQueue where ElementType: Comparable {
 
-    /**
-     Generic comparison function for Comparable types that provides for max value ordering
+  /**
+   Generic comparison function for Comparable types that provides for max value ordering
 
-     - parameter lhs: first value to compare
-     - parameter rhs: second value to compare
-     - returns: true if first value >= second value
-     */
-    public static func maxComp(_ lhs: ElementType, _ rhs: ElementType) -> Bool { lhs >= rhs }
+   - parameter lhs: first value to compare
+   - parameter rhs: second value to compare
+   - returns: true if first value >= second value
+   */
+  public static func maxComp(_ lhs: ElementType, _ rhs: ElementType) -> Bool { lhs >= rhs }
 
-    /**
-     Generic comparison function for Comparable types that provides for min value ordering
+  /**
+   Generic comparison function for Comparable types that provides for min value ordering
 
-     - parameter lhs: first value to compare
-     - parameter rhs: second value to compare
-     - returns: true if first value <= second value
-     */
-    public static func minComp(_ lhs: ElementType, _ rhs: ElementType) -> Bool { lhs <= rhs }
+   - parameter lhs: first value to compare
+   - parameter rhs: second value to compare
+   - returns: true if first value <= second value
+   */
+  public static func minComp(_ lhs: ElementType, _ rhs: ElementType) -> Bool { lhs <= rhs }
 
-    /**
-     Factory method for creating a PriorityQueue with max-value ordering
+  /**
+   Factory method for creating a PriorityQueue with max-value ordering
 
-     - parameter args: values to add to the queue
-     - returns: the new PriorityQueue instance
-     */
-    static public func maxOrdering(_ args: ElementType...) -> PriorityQueue { PriorityQueue(maxComp, values: args) }
+   - parameter args: values to add to the queue
+   - returns: the new PriorityQueue instance
+   */
+  static public func maxOrdering(_ args: ElementType...) -> PriorityQueue { PriorityQueue(maxComp, values: args) }
 
-    /**
-     Factory method for creating a PriorityQueue with min-value ordering
+  /**
+   Factory method for creating a PriorityQueue with min-value ordering
 
-     - parameter args: values to add to the queue
-     - returns: the new PriorityQueue instance
-     */
-    static public func minOrdering(_ args: ElementType...) -> PriorityQueue { PriorityQueue(minComp, values: args) }
+   - parameter args: values to add to the queue
+   - returns: the new PriorityQueue instance
+   */
+  static public func minOrdering(_ args: ElementType...) -> PriorityQueue { PriorityQueue(minComp, values: args) }
 
-    /**
-     Convenience constructor for a PriorityQueue with min-value ordering.
+  /**
+   Convenience constructor for a PriorityQueue with min-value ordering.
 
-     - parameter args: initial values to add to the queue
-     */
-    public init(_ args: ElementType...) {
-        isOrdered = Self.minComp
-        args.forEach { push($0) }
-    }
-}
-
-extension PriorityQueue {
-
-    mutating func replace(_ value: ElementType) -> ElementType {
-        let returnItem = heap[0]
-        heap[0] = value
-        siftUp(index: 0)
-        return returnItem
-    }
+   - parameter args: initial values to add to the queue
+   */
+  public init(_ args: ElementType...) {
+    isOrdered = Self.minComp
+    args.forEach { push($0) }
+  }
 }
--- Tests/PriorityQueueTests/PriorityQueueTests.swift
+++ Tests/PriorityQueueTests/PriorityQueueTests.swift
@@ -7,125 +7,133 @@ import XCTest
 
 final class PriorityQueueTests: XCTestCase {
 
-    func testMinQueue() {
-        var pq = PriorityQueue(1, 3, 5, 7, 9, 2, 4, 6, 8)
-        XCTAssertEqual(pq.count, 9)
-        XCTAssertEqual(pq.first, 1)
-        XCTAssertFalse(pq.isEmpty)
+  func testMinQueue() {
+    var pq = PriorityQueue(1, 3, 5, 7, 9, 2, 4, 6, 8)
+    XCTAssertEqual(pq.count, 9)
+    XCTAssertEqual(pq.first, 1)
+    XCTAssertFalse(pq.isEmpty)
 
-        XCTAssertEqual(pq.first, 1)
-        XCTAssertEqual(pq.pop()!, 1)
-        XCTAssertEqual(pq.pop()!, 2)
-        XCTAssertEqual(pq.pop()!, 3)
-        XCTAssertEqual(pq.pop()!, 4)
-        XCTAssertEqual(pq.pop()!, 5)
-        XCTAssertEqual(pq.pop()!, 6)
-        XCTAssertEqual(pq.pop()!, 7)
-        XCTAssertEqual(pq.pop()!, 8)
-        XCTAssertEqual(pq.pop()!, 9)
+    XCTAssertEqual(pq.first, 1)
+    XCTAssertEqual(pq.pop()!, 1)
+    XCTAssertEqual(pq.pop()!, 2)
+    XCTAssertEqual(pq.pop()!, 3)
+    XCTAssertEqual(pq.pop()!, 4)
+    XCTAssertEqual(pq.pop()!, 5)
+    XCTAssertEqual(pq.pop()!, 6)
+    XCTAssertEqual(pq.pop()!, 7)
+    XCTAssertEqual(pq.pop()!, 8)
+    XCTAssertEqual(pq.pop()!, 9)
 
-        XCTAssertEqual(pq.count, 0)
-        XCTAssertNil(pq.pop())
-        XCTAssertNil(pq.first)
-        XCTAssertTrue(pq.isEmpty)
+    XCTAssertEqual(pq.count, 0)
+    XCTAssertNil(pq.pop())
+    XCTAssertNil(pq.first)
+    XCTAssertTrue(pq.isEmpty)
+  }
+
+  func testMaxQueue() {
+    var pq = PriorityQueue(compare: PriorityQueue.maxComp, 1, 3, 5, 7, 9, 2, 4, 6, 8)
+    XCTAssertEqual(pq.count, 9)
+    XCTAssertEqual(pq.pop()!, 9)
+    XCTAssertEqual(pq.pop()!, 8)
+    XCTAssertEqual(pq.pop()!, 7)
+    XCTAssertEqual(pq.pop()!, 6)
+    XCTAssertEqual(pq.pop()!, 5)
+    XCTAssertEqual(pq.pop()!, 4)
+    XCTAssertEqual(pq.pop()!, 3)
+    XCTAssertEqual(pq.pop()!, 2)
+    XCTAssertEqual(pq.pop()!, 1)
+    XCTAssertEqual(pq.count, 0)
+    XCTAssertEqual(pq.pop(), nil)
+  }
+
+  func testPush() {
+    var pq = PriorityQueue.minOrdering(1, 3, 5, 7, 9)
+    print(pq)
+    pq.push(2)
+    print("init: \(pq)")
+    XCTAssertEqual(pq.count, 6)
+    XCTAssertEqual(pq.pop()!, 1)
+    print("after pop1: \(pq)")
+    XCTAssertEqual(pq.pop()!, 2)
+    print("after pop2: \(pq)")
+    XCTAssertEqual(pq.pop()!, 3)
+    print("after pop3: \(pq)")
+
+    pq.push(1)
+    print("after push: \(pq)")
+    XCTAssertEqual(pq.pop()!, 1)
+    print("after pop4: \(pq)")
+    XCTAssertEqual(pq.pop()!, 5)
+    print("after pop5: \(pq)")
+  }
+
+  func testRemoveAll() {
+    var pq = PriorityQueue(1, 3, 5, 7, 9)
+    XCTAssertEqual(pq.count, 5)
+    pq.removeAll()
+    XCTAssertEqual(pq.count, 0)
+  }
+
+  func testContains() {
+    let pq = PriorityQueue(1, 3, 5, 7, 9)
+    XCTAssertTrue(pq.contains(5))
+    XCTAssertFalse(pq.contains(6))
+  }
+
+  func testIteration() {
+    var pq = PriorityQueue(9, 8, 7, 1, 2, 3, 6, 4, 5)
+    XCTAssertEqual(pq.count, 9)
+    print(pq)
+    var counter = 1
+    pq.forEach {
+      XCTAssertEqual($0, counter)
+      counter += 1
     }
 
-    func testMaxQueue() {
-        var pq = PriorityQueue(compare: PriorityQueue.maxComp, 1, 3, 5, 7, 9, 2, 4, 6, 8)
-        XCTAssertEqual(pq.count, 9)
-        XCTAssertEqual(pq.pop()!, 9)
-        XCTAssertEqual(pq.pop()!, 8)
-        XCTAssertEqual(pq.pop()!, 7)
-        XCTAssertEqual(pq.pop()!, 6)
-        XCTAssertEqual(pq.pop()!, 5)
-        XCTAssertEqual(pq.pop()!, 4)
-        XCTAssertEqual(pq.pop()!, 3)
-        XCTAssertEqual(pq.pop()!, 2)
-        XCTAssertEqual(pq.pop()!, 1)
-        XCTAssertEqual(pq.count, 0)
-        XCTAssertEqual(pq.pop(), nil)
-    }
+    print(pq)
+    XCTAssertEqual(pq.count, 0)
+  }
 
-    func testPush() {
-        var pq = PriorityQueue.minOrdering(1, 3, 5, 7, 9)
-        pq.push(2)
-        XCTAssertEqual(pq.count, 6)
-        XCTAssertEqual(pq.pop()!, 1)
-        XCTAssertEqual(pq.pop()!, 2)
-        XCTAssertEqual(pq.pop()!, 3)
+  func testReplaceMinOrder() {
+    var pq = PriorityQueue.minOrdering(1, 3, 5, 7, 9)
+    XCTAssertEqual(pq.count, 5)
+    XCTAssertEqual(pq.pop()!, 1)
+    XCTAssertEqual(pq.pop()!, 3)
+    XCTAssertEqual(5, pq.replace(8))
+    XCTAssertEqual(pq.pop()!, 7)
+    XCTAssertEqual(pq.pop()!, 8)
+    XCTAssertEqual(pq.pop()!, 9)
+    XCTAssertEqual(pq.count, 0)
+  }
 
-        pq.push(1)
-        XCTAssertEqual(pq.pop()!, 1)
-        XCTAssertEqual(pq.pop()!, 5)
-    }
+  func testReplaceMaxOrder() {
+    var pq = PriorityQueue.maxOrdering(1, 3, 5, 7, 9)
+    XCTAssertEqual(pq.count, 5)
+    XCTAssertEqual(pq.pop()!, 9)
+    XCTAssertEqual(pq.pop()!, 7)
+    XCTAssertEqual(5, pq.replace(4))
+    XCTAssertEqual(pq.pop()!, 4)
+    XCTAssertEqual(pq.pop()!, 3)
+    XCTAssertEqual(pq.pop()!, 1)
+    XCTAssertEqual(pq.count, 0)
+  }
 
-    func testRemoveAll() {
-        var pq = PriorityQueue(1, 3, 5, 7, 9)
-        XCTAssertEqual(pq.count, 5)
-        pq.removeAll()
-        XCTAssertEqual(pq.count, 0)
-    }
+  class Entry: Comparable {
+    static func < (lhs: PriorityQueueTests.Entry, rhs: PriorityQueueTests.Entry) -> Bool { lhs.weight < rhs.weight }
+    static func == (lhs: PriorityQueueTests.Entry, rhs: PriorityQueueTests.Entry) -> Bool { lhs === rhs }
 
-    func testContains() {
-        let pq = PriorityQueue(1, 3, 5, 7, 9)
-        XCTAssertTrue(pq.contains(5))
-        XCTAssertFalse(pq.contains(6))
-    }
+    var weight: Int
+    init(_ weight: Int) { self.weight = weight }
+  }
 
-    func testIteration() {
-        var pq = PriorityQueue(9, 8, 7, 1, 2, 3, 6, 4, 5)
-        XCTAssertEqual(pq.count, 9)
-        print(pq)
-        var counter = 1
-        pq.forEach {
-            XCTAssertEqual($0, counter)
-            counter += 1
-        }
-
-        print(pq)
-        XCTAssertEqual(pq.count, 0)
-    }
-
-    func testReplaceMinOrder() {
-        var pq = PriorityQueue.minOrdering(1, 3, 5, 7, 9)
-        XCTAssertEqual(pq.count, 5)
-        XCTAssertEqual(pq.pop()!, 1)
-        XCTAssertEqual(pq.pop()!, 3)
-        XCTAssertEqual(5, pq.replace(8))
-        XCTAssertEqual(pq.pop()!, 7)
-        XCTAssertEqual(pq.pop()!, 8)
-        XCTAssertEqual(pq.pop()!, 9)
-        XCTAssertEqual(pq.count, 0)
-    }
-
-    func testReplaceMaxOrder() {
-        var pq = PriorityQueue.maxOrdering(1, 3, 5, 7, 9)
-        XCTAssertEqual(pq.count, 5)
-        XCTAssertEqual(pq.pop()!, 9)
-        XCTAssertEqual(pq.pop()!, 7)
-        XCTAssertEqual(5, pq.replace(4))
-        XCTAssertEqual(pq.pop()!, 4)
-        XCTAssertEqual(pq.pop()!, 3)
-        XCTAssertEqual(pq.pop()!, 1)
-        XCTAssertEqual(pq.count, 0)
-    }
-
-    class Entry: Comparable {
-        static func < (lhs: PriorityQueueTests.Entry, rhs: PriorityQueueTests.Entry) -> Bool { lhs.weight < rhs.weight }
-        static func == (lhs: PriorityQueueTests.Entry, rhs: PriorityQueueTests.Entry) -> Bool { lhs === rhs }
-
-        var weight: Int
-        init(_ weight: Int) { self.weight = weight }
-    }
-
-    static var allTests = [
-        ("testMinQueue", testMinQueue),
-        ("testMaxQueue", testMaxQueue),
-        ("testPush", testPush),
-        ("testRemoveAll", testRemoveAll),
-        ("testContains", testContains),
-        ("testIteration", testIteration),
-        ("testReplaceMinOrder", testReplaceMinOrder),
-        ("testReplaceMaxOrder", testReplaceMaxOrder),
-    ]
+  static var allTests = [
+    ("testMinQueue", testMinQueue),
+    ("testMaxQueue", testMaxQueue),
+    ("testPush", testPush),
+    ("testRemoveAll", testRemoveAll),
+    ("testContains", testContains),
+    ("testIteration", testIteration),
+    ("testReplaceMinOrder", testReplaceMinOrder),
+    ("testReplaceMaxOrder", testReplaceMaxOrder),
+  ]
 }
